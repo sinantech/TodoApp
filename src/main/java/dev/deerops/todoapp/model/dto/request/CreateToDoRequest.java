3a5582ad96229154838c942196b6aa4b3ac25916
sinantech <sinancemozer@gmail.com>
2024-11-04 12:00:15 +0300

    feat:create todo request changed with record class

--- src/main/java/dev/deerops/todoapp/model/dto/request/CreateToDoRequest.java
+++ src/main/java/dev/deerops/todoapp/model/dto/request/CreateToDoRequest.java
@@ -1,18 +1,16 @@
 package dev.deerops.todoapp.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class CreateToDoRequest {
-    @NotBlank
-    private String title;
-    @NotBlank
-    private String description;
+
+public record CreateToDoRequest(@NotBlank String title, @NotBlank String description) {
+
+    public CreateToDoRequest {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Title cannot be blank");
+        }
+        if (description == null || description.isBlank()) {
+            throw new IllegalArgumentException("Description cannot be blank");
+        }
+    }
 }
